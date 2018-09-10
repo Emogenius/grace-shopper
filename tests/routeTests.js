d8e7b2fd20a7ce353975bfdc5b92cad4296a564d
Kristin <kristinnharper@gmail.com>
2018-09-10 19:59:28 -0400

    model test rough drafts added. some framework for future route test sketched out

---
+++ tests/routeTests.js
@@ -0,0 +1,17 @@
+// 'use strict'
+
+// // Assertions
+// const chai = require('chai')
+// const expect = chai.expect
+// const chaiThings = require('chai-things')
+// chai.use(chaiThings)
+
+// const db = require('../server/db/models')
+// const User = db.model('user')
+// const Product = db.model('product')
+// const Order = db.model('order')
+// const Review = db.model('review')
+
+// // >>>>>>> eventual Routes tests will need
+// // const app = require('../server/app')
+// // const agent = require('supertest')(app)
