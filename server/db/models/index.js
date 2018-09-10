cf2283c46b2dc97146cf12aa1a8b2614329b9e74
evie33 <evieren90@gmail.com>
2018-09-10 16:51:24 -0400

    done with associations  and working on order model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,21 @@
 const User = require('./user')
+const Order = require('./order')
+const Product = require('./product')
+const Review = require('./review')
+
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+User.hasMany(Review)
+Review.belongsTo(User)
+
+Product.hasMany(Review)
+Review.belongsTo(Product)
+
+Order.belongsTohMany(Product,{through: 'order-product'})
+Product.belongsTohMany(Order,{thtought: 'order-product'})
+
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +31,5 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User, Order, Product, Review
 }
