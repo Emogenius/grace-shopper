25bf6e1650473996d7c7f861ad8dedd2085deff7
Esther Park <estherspark91@gmail.com>
2018-09-11 16:56:06 -0400

    pushing before pulling fixed seed file

--- client/components/user/user-home.js
+++ client/components/user/user-home.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {logout} from './store'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {email, handleClick} = props
 
-  //
+  // if there is no user aka email -EP
+  if (!email) {
+    return <Redirect to="/" />
+  }
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <div>
+        <h3>Welcome, {email}</h3>
+      </div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
@@ -20,14 +32,25 @@ export const UserHome = props => {
 /**
  * CONTAINER
  */
-// adding ownProps as second parameter -EP
-const mapState = (state, ownProps) => {
+const mapState = state => {
   return {
     email: state.user.email
   }
 }
 
-export default connect(mapState)(UserHome)
+// adding mapDispatch to access history and handleClick -EP
+// Q: how can I change this to async await?
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    handleClick() {
+      dispatch(logout()).then(() => {
+        ownProps.history.push('/')
+      })
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(UserHome)
 
 /**
  * PROP TYPES
