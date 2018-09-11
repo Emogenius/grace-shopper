5ec8bde5880a7fda75e06dda719853ba3e01cbb5
evie33 <evieren90@gmail.com>
2018-09-11 12:47:16 -0400

    finished all product and single produt route

--- tests/routeTests.js
+++ tests/routeTests.js
@@ -1,22 +1,21 @@
-// 'use strict'
+'use strict'
 
-// // Assertions
-// const chai = require('chai')
-// const expect = chai.expect
-// const chaiThings = require('chai-things')
-// chai.use(chaiThings)
+// Assertions
+const chai = require('chai')
+const expect = chai.expect
+const chaiThings = require('chai-things')
+chai.use(chaiThings)
 
-// const db = require('../server/db/models')
-// const User = db.model('user')
-// const Product = db.model('product')
-// const Order = db.model('order')
-// const Review = db.model('review')
+const db = require('../server/db/models')
+const User = db.model('user')
+const Product = db.model('product')
+const Order = db.model('order')
+const Review = db.model('review')
 
-// // >>>>>>> eventual Routes tests will need
-// // const app = require('../server/app')
-// // const agent = require('supertest')(app)
+// >>>>>>> eventual Routes tests will need
+// const app = require('../server/app')
+// const agent = require('supertest')(app)
 
-// defined in ../server/routes/campuses.js
 // describe('Campus routes', () => {
 //   let storedCampuses
 
