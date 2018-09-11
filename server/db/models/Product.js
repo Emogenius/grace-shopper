34acdb358cf89a67530a40b9b0ab5ffe829928f1
Esther Park <estherspark91@gmail.com>
2018-09-11 10:14:22 -0400

    discovered typo in index.js with associations-now fixed, also added some notes on models/product.js for future changes or reference

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -5,28 +5,34 @@ const Product = db.define('products', {
   title: {
     type: Sequelize.STRING,
     allowNull: false,
-    validate: {notEmpty: true}
+    validate: {
+      notEmpty: true
+    }
   },
   decription: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-    validate: {notEmpty: true}
+    type: Sequelize.TEXT
+    // allowNull: false,
+    // validate: {notEmpty: true}
   },
   price: {
-    type: Sequelize.DECIMAL,
-    validate: {min: 0}
+    type: Sequelize.DECIMAL, //Seqeulize.INTEGER
+    allowNull: false,
+    validate: {min: 0} //defaultValue: 1
   },
   inventoryQuantity: {
     type: Sequelize.INTEGER,
     validate: {min: 0}
   },
   category: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, //Sequelize.ARRAY(Sequelize.STRING) ?
     allowNull: false,
-    validate: {notEmpty: true}
+    validate: {
+      notEmpty: true
+    }
   },
   imageUrl: {
     type: Sequelize.STRING,
+    //'/smiling_face_emoji.png' sth
     defaultValue:
       'http://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Face_Emoji_grande.png?v=1480481056'
   }
