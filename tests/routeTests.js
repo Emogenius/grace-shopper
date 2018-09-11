5f7561995363cd1eaca809104ab924b2f3ab5289
Kristin <kristinnharper@gmail.com>
2018-09-10 20:05:25 -0400

    fixed some formatting problems on model tests, added more starter code routes

--- tests/routeTests.js
+++ tests/routeTests.js
@@ -15,3 +15,39 @@
 // // >>>>>>> eventual Routes tests will need
 // // const app = require('../server/app')
 // // const agent = require('supertest')(app)
+
+// defined in ../server/routes/campuses.js
+// describe('Campus routes', () => {
+//   let storedCampuses
+
+//   const campusData = [
+//     {
+//       name: 'Grace Hopper'
+//     },
+//     {
+//       name: 'Fullstack Academy'
+//     }
+//   ]
+
+//   beforeEach(async () => {
+//     const createdCampuses = await Campus.bulkCreate(campusData)
+//     storedCampuses = createdCampuses.map(campus => campus.dataValues)
+//   })
+
+//   // Route for fetching all campuses
+//   describe('GET `/api/campuses`', () => {
+//     it('serves up all Campuses', async () => {
+//       const response = await agent.get('/api/campuses').expect(200)
+//       expect(response.body).to.have.length(2)
+//       expect(response.body[0].name).to.equal(storedCampuses[0].name)
+//     })
+//   })
+
+//   // Route for fetching a single campus
+//   describe('GET `/api/campuses/:id`', () => {
+//     it('serves up a single Campus by its id', async () => {
+//       const response = await agent.get('/api/campuses/2').expect(200)
+//       expect(response.body.name).to.equal('Fullstack Academy')
+//     })
+//   })
+// })
