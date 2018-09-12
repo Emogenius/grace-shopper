b2f9bfd251101310ed7228352c3a79034928d740
evie33 <evieren90@gmail.com>
2018-09-12 18:58:50 -0400

    update view to not have side effect on other view

--- client/app.js
+++ client/app.js
@@ -2,15 +2,43 @@ import React from 'react'
 import {Navbar} from './components'
 import {Sidebar} from './components'
 import Routes from './routes'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 
-function App() {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+})
+
+function App({classes}) {
   return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <Routes />
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Navbar />
+        </Grid>
+
+        <Grid item xs={2}>
+          <Sidebar />
+        </Grid>
+
+        <Grid item xs={9}>
+          <Routes />
+        </Grid>
+      </Grid>
     </div>
   )
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(App)
