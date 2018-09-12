740548237253343ae488207e880d8e9350a73be5
Jasmine English <thisjasmine0@gmail.com>
2018-09-11 21:31:05 -0400

    added cart property to session

--- server/index.js
+++ server/index.js
@@ -63,6 +63,13 @@ const createApp = () => {
   app.use(passport.initialize())
   app.use(passport.session())
 
+  // cart cookie
+  app.use((req, res, next) => {
+    if (!req.session.cart) {
+      req.session.cart = {}
+    }
+  })
+
   // auth and api routes
   app.use('/auth', require('./auth'))
   app.use('/api', require('./api'))
