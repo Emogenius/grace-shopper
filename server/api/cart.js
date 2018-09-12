29ddcccbba00a83e72553ef13f3f5360c3680b63
Jasmine English <thisjasmine0@gmail.com>
2018-09-11 22:39:28 -0400

    merging

---
+++ server/api/cart.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  try {
+    res.json(req.session.cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:productId', (req, res, next) => {
+  try {
+    req.session.cart = req.session.filter(
+      product => product.id !== req.params.productId
+    )
+    res.json(req.session.cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// router.put('/:productId', (req, res, next) => {
+//   try {
+//     req.session.cart = req.session.filter(
+//       product => product.id === req.params.productId
+//     )
+//   } catch (err) {
+//     next(err)
+//   }
+// })
