cd92691d72c67b1c813213006d78613539c4ce2a
Kristin <kristinnharper@gmail.com>
2018-09-17 15:14:01 -0400

    doesnt totally work but form with submit button is there

--- client/components/cart/CheckOut.js
+++ client/components/cart/CheckOut.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-const stripe = Stripe('pk_test_bmkjpUhNBrtmnYPdTdrNSkLa')
-const elements = stripe.elements()
-// const CheckOut = props => {
-//   //real payment ?
+import {CardElement, injectStripe} from 'react-stripe-elements'
+
+// class CheckoutForm extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.submit = this.submit.bind(this)
+//   }
+
+//   async submit(ev) {
+//     console.log('User clicked submit')
+//   }
+
+//   render() {
+//     return (
+//       <div className="checkout">
+//         <p>Would you like to complete the purchase?</p>
+//         <CardElement />
+//         <button onClick={this.submit}>Send</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default injectStripe(CheckoutForm)
 
 class CheckOut extends Component {
   constructor(props) {
@@ -14,60 +34,58 @@ class CheckOut extends Component {
 
   render() {
     return (
-      // <form>
-      //   <div class="form-group">
-      //     <label for="formGroupExampleInput">Ship to:</label>
-      //     <input
-      //       type="text"
-      //       class="form-control"
-      //       id="formGroupExampleInput"
-      //       placeholder="Name of recipient"
-      //     />
-      //   </div>
-      //   <div class="form-group">
-      //     <label for="formGroupExampleInput2">Shipping Address</label>
-      //     <input
-      //       type="text"
-      //       class="form-control"
-      //       id="formGroupExampleInput2"
-      //       placeholder="Street address, city, state, zipcode"
-      //     />
-      //   </div>
-      //   <div class="form-group">
-      //     <label for="formGroupExampleInput">Billing Address:</label>
-      //     <input
-      //       type="text"
-      //       class="form-control"
-      //       id="formGroupExampleInput"
-      //       placeholder="Street address, city, state, zipcode"
-      //     />
-      //   </div>
-      // </form>
-      // <form action="/charge" method="post" id="payment-form">
-      //   <div class="form-row">
-      //     <label for="card-element">Credit or debit card</label>
-      //     <div id="card-element" />
-      //     !-- A Stripe Element will be inserted here. --
-      //     <div id="card-errors" role="alert" />
-      //     {/* !-- Used to display Element errors. -- */}
-      //   </div>
-
-      //   <button>Submit Payment</button>
-      // </form>
-      <form action="your-server-side-code" method="POST">
-        <script
-          src="https://checkout.stripe.com/checkout.js"
-          class="stripe-button"
-          data-key="pk_test_bmkjpUhNBrtmnYPdTdrNSkLa"
-          data-amount="999"
-          data-name="Demo Site"
-          data-description="Widget"
-          data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-          data-locale="auto"
-        />
+      <form>
+        <h1> Shipping & Payment Information </h1>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput">Ship to:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Name of recipient"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput2">Shipping Address:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Street address, apt#,city, state, zipcode"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput">Billing Address:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Street address, apt#, city, state, zipcode"
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="card-element">Credit or debit card</label>
+          <div id="card-element">
+            -- A Stripe Element will be inserted here. --
+          </div>
+          <div id="card-errors" role="alert" />
+        </div>
+        <button>Submit Payment</button>
       </form>
     )
   }
 }
 
 export default connect()(CheckOut)
+
+/* <form action="your-server-side-code" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_bmkjpUhNBrtmnYPdTdrNSkLa"
+    data-amount="999"
+    data-name="Demo Site"
+    data-description="Widget"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto">
+  </script>
+</form> */
