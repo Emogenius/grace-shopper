65c141bd962ea07e5c5774560a95034f0cd1d15a
evie33 <evieren90@gmail.com>
2018-09-18 17:13:54 -0400

    user view review button works

--- client/components/User/UserHome.js
+++ client/components/User/UserHome.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {getUserReviews} from '../../store/reviewReducer'
 import StarRatingComponent from 'react-star-rating-component'
 import EditReview from '../reviews/EditReview'
 /**
@@ -11,12 +10,15 @@ class UserHome extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      updateClick: false
+      updateClick: false,
+      orderClick: false,
+      reviewClick: false
     }
     this.updateForm = this.updateForm.bind(this)
-    //this.handleAdd = this.handleAdd.bind(this)
+    this.orderList = this.orderList.bind(this)
+    this.reviewList = this.reviewList.bind(this)
   }
-  // const revs = props.reviews.filter(rev => rev.userId === props.id)
+
   updateForm() {
     if (!this.state.updateClick) {
       this.setState({updateClick: true})
@@ -24,8 +26,25 @@ class UserHome extends Component {
       this.setState({updateClick: false})
     }
   }
+
+  orderList() {
+    if (!this.state.orderClick) {
+      this.setState({orderClick: true})
+    } else {
+      this.setState({orderClick: false})
+    }
+  }
+
+  reviewList() {
+    if (!this.state.reviewClick) {
+      this.setState({reviewClick: true})
+    } else {
+      this.setState({reviewClick: false})
+    }
+  }
   render() {
-    console.log(this.props.reviews)
+    console.log(this.props.orders, '--------->>>>')
+
     const revs = this.props.reviews.filter(rev => rev.userId === this.props.id)
     return (
       <div>
@@ -34,46 +53,112 @@ class UserHome extends Component {
           <h2>email: {this.props.user.email}</h2>
           <h2>mailing address: {this.props.user.address}</h2>
           <h2>phone: {this.props.user.phoneNumber}</h2>
-          <h3> reviews:</h3>
-          <ul>
-            {revs.map(rev => (
-              <li key={rev.id}>
-                <h3>
-                  <em> {rev.product.title}</em>
-                </h3>
-                <h3> "{rev.title}"</h3>
-                <h4>{rev.description}</h4>
-                <h1>
-                  <StarRatingComponent
-                    name="rate1"
-                    starColor={'DeepPink'}
-                    starCount={5}
-                    value={rev.rating}
-                  />
-                </h1>
-                <h3> rating: {rev.rating}</h3>
-                <button
-                  type="button"
-                  className="btn btn-outline-warning"
-                  onClick={() => {
-                    this.updateForm()
-                  }}
-                >
-                  Edit
-                </button>
-                {this.state.updateClick ? (
-                  <EditReview review={rev} updateForm={this.updateForm} />
-                ) : null}
-                <button type="button" className="btn btn-outline-warning">
-                  {/* onClick={() => { */}
-                  {/* //   this.props.remove(emoji.id)
-                // }} */}
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
         </div>
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          onClick={() => {
+            this.reviewList()
+          }}
+        >
+          My Reviews
+        </button>
+        {this.state.reviewClick ? (
+          <div>
+            <h3> reviews:</h3>
+            <ul>
+              {revs.map(rev => (
+                <li key={rev.id}>
+                  <h3>
+                    <em> {rev.product.title}</em>
+                  </h3>
+                  <h3> "{rev.title}"</h3>
+                  <h4>{rev.description}</h4>
+                  <h1>
+                    <StarRatingComponent
+                      name="rate1"
+                      starColor="DeepPink"
+                      starCount={5}
+                      value={rev.rating}
+                    />
+                  </h1>
+                  <h3> rating: {rev.rating}</h3>
+                  <button
+                    type="button"
+                    className="btn btn-outline-warning"
+                    onClick={() => {
+                      this.updateForm()
+                    }}
+                  >
+                    Edit
+                  </button>
+                  {this.state.updateClick ? (
+                    <EditReview review={rev} updateForm={this.updateForm} />
+                  ) : null}
+                  <button type="button" className="btn btn-outline-warning">
+                    {/* onClick={() => { */}
+                    {/* //   this.props.remove(emoji.id)
+                // }} */}
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          onClick={() => {
+            this.orderList()
+          }}
+        >
+          My Orders
+        </button>
+        {this.state.orderClick ? (
+          <div>
+            <h3> reviews:</h3>
+            <ul>
+              {revs.map(rev => (
+                <li key={rev.id}>
+                  <h3>
+                    <em> {rev.product.title}</em>
+                  </h3>
+                  <h3> "{rev.title}"</h3>
+                  <h4>{rev.description}</h4>
+                  <h1>
+                    <StarRatingComponent
+                      name="rate1"
+                      starColor="DeepPink"
+                      starCount={5}
+                      value={rev.rating}
+                    />
+                  </h1>
+                  <h3> rating: {rev.rating}</h3>
+                  <button
+                    type="button"
+                    className="btn btn-outline-warning"
+                    onClick={() => {
+                      this.updateForm()
+                    }}
+                  >
+                    Edit
+                  </button>
+                  {this.state.updateClick ? (
+                    <EditReview review={rev} updateForm={this.updateForm} />
+                  ) : null}
+                  <button type="button" className="btn btn-outline-warning">
+                    {/* onClick={() => { */}
+                    {/* //   this.props.remove(emoji.id)
+                // }} */}
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
       </div>
     )
   }
@@ -90,18 +175,12 @@ const mapState = state => {
     user: state.user.current,
     id: state.user.current.id,
     selectedUser: state.reviews.selectedUser,
-    reviews: state.reviews.reviews
+    reviews: state.reviews.reviews,
+    orders: state.orders
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    gotReviews: id => dispatch(getReviews(id))
-    // gotOrders: id => dispatch(getOrders(id))
-  }
-}
-
-export default connect(mapState, mapDispatchToProps)(UserHome)
+export default connect(mapState)(UserHome)
 
 /**
  * PROP TYPES
