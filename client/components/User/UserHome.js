09c056b2f87a0b97790a6e0997c2f25b85851701
evie33 <evieren90@gmail.com>
2018-09-18 18:05:39 -0400

    admin detail and user detial

--- client/components/User/UserHome.js
+++ client/components/User/UserHome.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import StarRatingComponent from 'react-star-rating-component'
 import EditReview from '../reviews/EditReview'
+import {Link} from 'react-router-dom'
 /**
  * COMPONENT
  */
@@ -43,8 +44,8 @@ class UserHome extends Component {
     }
   }
   render() {
-    console.log(this.props.orders, '--------->>>>')
-
+    const user = this.props.user
+    const orders = this.props.orders
     const revs = this.props.reviews.filter(rev => rev.userId === this.props.id)
     return (
       <div>
@@ -63,6 +64,27 @@ class UserHome extends Component {
         >
           My Reviews
         </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          onClick={() => {
+            this.orderList()
+          }}
+        >
+          My Orders
+        </button>
+
+        {user.isAdmin ? (
+          <div>
+            <Link to="/orders">
+              <button type="button" className="btn btn-outline-warning">
+                List All User's Orders
+              </button>
+            </Link>
+          </div>
+        ) : null}
+
         {this.state.reviewClick ? (
           <div>
             <h3> reviews:</h3>
@@ -107,53 +129,15 @@ class UserHome extends Component {
           </div>
         ) : null}
 
-        <button
-          type="button"
-          className="btn btn-outline-dark"
-          onClick={() => {
-            this.orderList()
-          }}
-        >
-          My Orders
-        </button>
         {this.state.orderClick ? (
           <div>
-            <h3> reviews:</h3>
+            <h3> Orders:</h3>
             <ul>
-              {revs.map(rev => (
-                <li key={rev.id}>
+              {orders.map(each => (
+                <li key={each.id}>
                   <h3>
-                    <em> {rev.product.title}</em>
+                    Order #: {each.id} {'      '} Status: {each.isFulfill}
                   </h3>
-                  <h3> "{rev.title}"</h3>
-                  <h4>{rev.description}</h4>
-                  <h1>
-                    <StarRatingComponent
-                      name="rate1"
-                      starColor="DeepPink"
-                      starCount={5}
-                      value={rev.rating}
-                    />
-                  </h1>
-                  <h3> rating: {rev.rating}</h3>
-                  <button
-                    type="button"
-                    className="btn btn-outline-warning"
-                    onClick={() => {
-                      this.updateForm()
-                    }}
-                  >
-                    Edit
-                  </button>
-                  {this.state.updateClick ? (
-                    <EditReview review={rev} updateForm={this.updateForm} />
-                  ) : null}
-                  <button type="button" className="btn btn-outline-warning">
-                    {/* onClick={() => { */}
-                    {/* //   this.props.remove(emoji.id)
-                // }} */}
-                    Remove
-                  </button>
                 </li>
               ))}
             </ul>
@@ -169,14 +153,14 @@ class UserHome extends Component {
  */
 const mapState = state => {
   return {
-    // email: state.user.current.email,
-    // name: state.user.current.fullName,
     ...state,
     user: state.user.current,
     id: state.user.current.id,
     selectedUser: state.reviews.selectedUser,
     reviews: state.reviews.reviews,
-    orders: state.orders
+    orders: state.orders.orders.filter(each => {
+      return each.userId === state.user.current.id
+    })
   }
 }
 
